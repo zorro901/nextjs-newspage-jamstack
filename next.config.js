d783ebea99c9f2143c09f15aac2713bb91b6e294
User Name <zoro927@hotmail.com>
2022-05-25 12:40:46 +0900

    Image domain add

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['storage.googleapis.com']
+    domains: ['storage.googleapis.com','imageio.forbes.com','www.reuters.com']
   }
 }
 
