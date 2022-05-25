006d99092718094669b4c21a940c8eb1372dd38d
User Name <zoro927@hotmail.com>
2022-05-25 14:37:51 +0900

    image components fix

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -1,45 +1,49 @@
-import {client} from '../../libs/client'
-import Image from "next/image";
+import { client } from '../../libs/client'
+import Image from 'next/image'
+
 export default function NewsId({news}: any) {
-    const ondemandRevalidate = async () => {
-        await fetch('/api/revalidate').catch((error) => {
-            console.error('error', error);
-        });
-    };
-    return (
-        <>
-            <h1>{news.title}</h1>
-            {/*<p>{news.publishedAt}</p>*/}
+  const ondemandRevalidate = async () => {
+    await fetch('/api/revalidate').catch((error) => {
+      console.error('error', error)
+    })
+  }
+  return (
+    <>
+      <h1>{news.title}</h1>
+      {/*<p>{news.publishedAt}</p>*/}
 
-            <div
-                dangerouslySetInnerHTML={{
-                    __html: `${news.body}`,
-                }}
-            />
-            <Image src={`${news.imageSrc}`}
-                   width={300}
-                   height={300}
-            />
-
-            {/*<p>`NewsID : ${news.id}`</p>*/}
-            {/*<button onClick={ondemandRevalidate}>オンデマンドISR</button>*/}
-        </>
-    )
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${news.body}`
+        }}
+      />
+      <Image src={`${news.imageSrc}`}
+             // layout="fill"
+             height={300}
+             width={500}
+             objectFit="cover"
+             alt={'image'}
+             quality={70}
+      />
+      {/*<p>`NewsID : ${news.id}`</p>*/}
+      {/*<button onClick={ondemandRevalidate}>オンデマンドISR</button>*/}
+    </>
+  )
 }
 
 export const getStaticPaths = async () => {
-    const data = await client.get({endpoint: "news"})
-    const paths = data.contents.map((content: { id: any }) => `/news/${content.id}`)
-    return {paths, fallback: false}
+  const data = await client.get({endpoint: 'news',queries:{limit:50}})
+  const paths = data.contents.map((content: { id: any }) => `/news/${content.id}`)
+  return {paths, fallback: false}
 }
 
 export const getStaticProps = async (context: any) => {
-    const id = context.params.id
-    const data = await client.get({endpoint: "news", contentId: id})
-    return {
-        props: {
-            news: data
-        },
-        revalidate: 60,
-    }
+  const id = context.params.id
+  const data = await client.get({endpoint: 'news', contentId: id})
+  return {
+    props: {
+      news: data
+    },
+    revalidate: 60
+  }
 }
