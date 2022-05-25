91870c22e6098a2b5bf041c85e6b4087a63dbe26
User Name <zoro927@hotmail.com>
2022-05-25 12:41:04 +0900

    get article limit change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,39 @@
 import Link from 'next/link'
-import {client} from '../libs/client'
-import Image from "next/image";
+import { client } from '../libs/client'
+import Image from 'next/image'
 
 export default function Home({news}: any) {
-    return (
-        <>
-            {/*<Image src={"https://storage.googleapis.com/afs-prod/media/448c0655aee54d1587b06761bc57a575/1000.jpeg"}*/}
-            {/*       width={300}*/}
-            {/*       height={300}*/}
-            {/*       alt={"aaa"}*/}
-            {/*/>*/}
+  return (
+    <>
+      {/*<Image src={"https://storage.googleapis.com/afs-prod/media/448c0655aee54d1587b06761bc57a575/1000.jpeg"}*/}
+      {/*       width={300}*/}
+      {/*       height={300}*/}
+      {/*       alt={"aaa"}*/}
+      {/*/>*/}
 
-            <ul>
-                {news.map((data: any) =>
-                    <li key={data.id}>
-                        <Link href={`/news/${data.id}`}>
-                            <a>{data.title}</a>
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </>
-    )
+      <ul>
+        {news.map((data: any) =>
+          <li key={data.id}>
+            <Link href={`/news/${data.id}`}>
+              <a>{data.title}</a>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </>
+  )
 }
 export const getStaticProps = async () => {
-    const data = await client.get({endpoint: 'news'})
-    return {
-        props: {
-            news: data.contents
-        },
-        revalidate: 60,
+  const data = await client.get({
+    endpoint: 'news', queries: {
+      offset: 0,
+      limit: 100
     }
+  })
+  return {
+    props: {
+      news: data.contents
+    },
+    revalidate: 60
+  }
 }
